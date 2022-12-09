a84401cde22ac590c439e01f4eb3e2c67bdf33aa
Jefferson Cajas <38546533+impostorkemar@users.noreply.github.com>
2022-12-09 07:39:33 -0500

    Final version

--- FASTAPI-MYSQL-RESTAPI-PROYECTO/app.py
+++ FASTAPI-MYSQL-RESTAPI-PROYECTO/app.py
@@ -18,14 +18,18 @@ origins =[
     "http://192.168.1.35:4200",
     "https://192.168.1.35:4200",
     "http://192.168.100.81:4200",
-    "https://192.168.1.81:4200"
+    "https://192.168.1.81:4200",
+    "https://main.d12wuq988la98b.amplifyapp.com",
+    "http://main.d12wuq988la98b.amplifyapp.com",
+    "https://main.d12wuq988la98b.amplifyapp.com:4200",
+    "http://main.d12wuq988la98b.amplifyapp.com:4200"
 
     
 ]
 #adding middleware uris
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
